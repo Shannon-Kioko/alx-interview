f50674e44d3f85e3729e3cfce5c6e3be3b94d2ea
Shannon Kioko <shannonkaluki@gmail.com>
2024-04-29 00:15:25 +0300

    Add task file for minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,15 @@
-def
+def minOperations(n):
+    if n <= 1:
+        return 0
+    
+    divisor = 2
+    number_of_operations = 0
+
+    while n > 1:
+        if n % divisor == 0:
+            n /= divisor
+            number_of_operations += divisor
+        else:
+            divisor += 1
+    
+    return number_of_operations
