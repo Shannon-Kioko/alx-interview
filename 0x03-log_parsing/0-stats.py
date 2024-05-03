829e8009b05f434b448f3db035e22a0c8f905950
Shannon-Kioko <shannonkaluki@gmail.com>
2024-05-03 05:17:16 +0300

    Add: task files

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,7 +4,8 @@ import datetime
 
 # Initialize variables to store metrics
 file_size_total = 0
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_code_counts = {200: 0, 301: 0, 400: 0,
+                      401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 
 try:
