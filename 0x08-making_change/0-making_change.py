0c97e9ed4f2e26e84625abbdff7bf0c12c13a9c4
Shannon-Kioko <shannonkaluki@gmail.com>
2024-06-10 15:56:40 +0300

    Update files with standard convetion

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
+import time
 """
 Python script to solve a dynamic programming problem
 """
+
+
 def makeChange(coins, total):
     """
-    Function to calculate the minimum number of coins needed to make a given total.
+    Function to calculate the minimum number of coins needed to make a
+    given total.
 
-    This function uses a dynamic programming approach to solve the problem. It creates a list dp where dp[i] 
-    represents the minimum number of coins needed to make a total of i. The function iterates over each coin, 
-    and for each coin it iterates over each total from 0 to the given total. If the current coin is less than 
-    or equal to the current total, it updates dp[i] to be the minimum of the current value of dp[i] and 
+    This function uses a dynamic programming approach to solve the problem.
+    It creates a list dp where dp[i] represents the minimum number
+    of coins needed to make a total of i. The function iterates over each coin,
+    and for each coin it iterates over each total from 0 to the given total.
+    If the current coin is less than or equal to the current total,
+    it updates dp[i] to be the minimum of the current value of dp[i] and
     dp[i-coin] + 1.
 
     Parameters:
-    coins (list of int): A list of integers representing the denominations of the coins.
+    coins (list of int): A list of integers representing the
+    denominations of the coins.
     total (int): The total amount for which we want to make change.
 
     Returns:
-    int: The minimum number of coins needed to make the given total. If it's not possible to make the total 
+    int: The minimum number of coins needed to make the given total.
+    If it's not possible to make the total
     with the given coins, it returns -1.
     """
     if not coins:
@@ -26,7 +34,7 @@ def makeChange(coins, total):
         return 0
     if total < min(coins):
         return -1
-    
+
     dp = [total+1 for i in range(total+1)]
     dp[0] = 0
 
@@ -35,5 +43,3 @@ def makeChange(coins, total):
             if coin <= i:
                 dp[i] = min(dp[i-coin]+1, dp[i])
     return dp[-1]
-
-print(makeChange([], 1))
