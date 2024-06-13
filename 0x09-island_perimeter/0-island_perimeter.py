20e28febf06fb37ccefe6ee081a02e00655f0004
Shannon Kioko <shannonkaluki@gmail.com>
2024-06-14 00:12:04 +0300

    Add: Task fil

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -29,12 +29,12 @@ def island_perimeter(grid):
 
     return perimeter
 
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))
+# if __name__ == "__main__":
+#     grid = [
+#         [0, 0, 0, 0, 0, 0],
+#         [0, 1, 0, 0, 0, 0],
+#         [0, 1, 0, 0, 0, 0],
+#         [0, 1, 1, 1, 0, 0],
+#         [0, 0, 0, 0, 0, 0]
+#     ]
+#     print(island_perimeter(grid))
