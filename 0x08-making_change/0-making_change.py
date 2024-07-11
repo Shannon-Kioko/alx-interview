1187bfe589a73e9bf3e6970c9bf4efe9e6731f53
Shannon Kioko <shannonkaluki@gmail.com>
2024-07-11 11:08:03 +0300

    Update: py files

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-""" Making changes """
+"""
+Python script to solve a dynamic programming problem
+"""
 
 
 def makeChange(coins, total):
-    """ Generate changes needed to reach total
-
-    Args:
-        coins ([List]): [List of Coins available]
-        total ([int]): [total amount needed]
+   """
+    Function to calculate the minimum number of coins needed to make a
+    given total.
+    This function uses a dynamic programming approach to solve the problem.
+    It creates a list dp where dp[i] represents the minimum number
+    of coins needed to make a total of i. The function iterates over each coin,
+    and for each coin it iterates over each total from 0 to the given total.
+    If the current coin is less than or equal to the current total,
+    it updates dp[i] to be the minimum of the current value of dp[i] and
+    dp[i-coin] + 1.
+    Parameters:
+    coins (list of int): A list of integers representing the
+    denominations of the coins.
+    total (int): The total amount for which we want to make change.
+    Returns:
+    int: The minimum number of coins needed to make the given total.
+    If it's not possible to make the total
+    with the given coins, it returns -1.
     """
 
 
