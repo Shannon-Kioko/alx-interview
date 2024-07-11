a57b0ff70147fbd2e755a4744c39ac038fa51616
Shannon Kioko <shannonkaluki@gmail.com>
2024-07-11 11:09:46 +0300

    Fix: Unexpected identation issues

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -26,17 +26,17 @@ def makeChange(coins, total):
     """
 
 
-if total <= 0:
-    return 0
-check = 0
-temp = 0
-coins.sort(reverse=True)
-for i in coins:
-    while check < total:
-        check += i
-        temp += 1
-    if check == total:
-        return temp
-    check -= i
-    temp -= 1
-return -1
+    if total <= 0:
+        return 0
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
